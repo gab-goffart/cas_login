c35effdacfa7cebc76893590375f7c1135aad25b
ggoffart <ggoffart@boreal-is.com>
2019-08-15 10:07:14 -0400

    added second request, will need to parse first request's HTMl to find the hidden inputs

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
 	"net/http"
+	"net/url"
+	"strconv"
+	"strings"
 )
 
 func main() {
@@ -20,30 +22,44 @@ func main() {
 	}
 
 	cookies := res.Cookies()
+	form := url.Values{}
+	form.Set("username", "gofg2301")
+	form.Set("password", "Goffart2006")
 
-	var cookieValues map[string]string
-	for _, cookie := range cookies {
-		cookieValues[cookie.Name] = cookie.Value
-	}
-
-	var form map[string]string
-	form["username"] = "gab.goffart@gmail.com"
-	form["password"] = "Goffart2006"
-
-	formBytes := []byte(form)
-
-	req, err := http.NewRequest("POST", "cas.usherbrooke.ca/login", bytes.NewBuffer(form))
+	req, err := http.NewRequest("POST", "https://cas.usherbrooke.ca/login", strings.NewReader(form.Encode()))
 	if err != nil {
-		fmt.Println("There was an error while trying to get to authentication service from UdeS")
+		fmt.Println("There was an error building the request")
 		return
 	}
 
-	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-	req.Header.Set("Cookies", string(cookieValues))
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Add("Content-Length", strconv.Itoa(len(form.Encode())))
+	for _, cookie := range cookies {
+		req.AddCookie(cookie)
+		fmt.Println("Cookie added : ", cookie)
+	}
 
 	fmt.Println("===========================================")
-	fmt.Println("Here is the response the server sent: ")
-	fmt.Println("Headers : ", res.Header)
-	fmt.Println("Cookies : ", res.Cookies())
+	fmt.Println("Sending request to authenticate")
 	fmt.Println("===========================================")
+
+	client := http.Client{}
+
+	res, err = client.Do(req)
+
+	if err != nil {
+		fmt.Println("Error trying to authenticate")
+		fmt.Println(err)
+		return
+	}
+	defer res.Body.Close()
+
+	if err != nil {
+		fmt.Println("Could not read body of response")
+		return
+	}
+
+	fmt.Println("response headers: ", res.Header)
+	fmt.Println("response status", res.Status)
+	fmt.Println("response cookies : ", res.Cookies())
 }
